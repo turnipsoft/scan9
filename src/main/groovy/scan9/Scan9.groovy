2e99a97de63f8a3bf7e860e4a7445598a7864445
Soren Hartvig <shartvig@nineconsult.dk>
2015-07-07 09:30:54 +0200

    first

---
+++ src/main/groovy/scan9/Scan9.groovy
@@ -0,0 +1,17 @@
+package scan9
+
+/**
+ * The runnable main class of Scan9
+ */
+public class Scan9 {
+
+
+    public static void main(String [] args) {
+        Scan9 scan9 = new Scan9()
+        scan9.scan()
+    }
+
+    public void scan() {
+
+    }
+}
